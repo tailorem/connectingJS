2d52be9a4b4036ac65110b1b7dab0d7d3b2fd9a1
Taylour <29444471+tailorem@users.noreply.github.com>
2018-07-24 21:15:00 +0000

    explores starter code

---
+++ test_script.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+console.log(pg.Client.Query);
+
+client.connect((err) => {
+  if (err) {
+  console.log("D'OH, A CONNECTION ERROR HAPPENED.");
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+    if (err) {
+      console.log("D'OH, AN ERROR HAPPENED.");
+      return console.error("error running query", err);
+    }
+    console.log("Hey, it worked?");
+    console.log(result); //output: 1
+    client.end();
+  });
+});
